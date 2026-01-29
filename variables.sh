0c7816f7acaaaffee8d6224c55a85dd22f780f38
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-28 23:08:37 -0500

    learning

---
+++ variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+PERSON1="$1" #no space between and variable should be uppercase
+PERSON2="$2" # we can replace the variable value while calling the script using $1,$2
+
+#$should be used while calling the variable
+echo "$PERSON1 : hi, how are you?"
+echo "$PERSON2 : i am fine, thank you! how about you?"
+echo "$PERSON1 : i am doing well, thanks for asking."    
+echo "$PERSON2 : great to hear that. what have you been up to lately?"
+echo "$PERSON1 : i've been working on some shell scripting projects. it's been fun!"
+echo "$PERSON2 : that's awesome! shell scripting is really useful for automating tasks."
+echo "$PERSON1 : absolutely! do you have any projects you're working on?"
+echo "$PERSON2 : yes, i'm trying to create a backup script for my important files."
+echo "$PERSON1 : sounds like a great idea. let me know if you need any help with it."
+echo "$PERSON2 : will do! thanks, vineeth."
