e9366d30de63ea8fcec773c2e4bedcb5efa7bb49
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-28 23:52:40 -0500

    learning

--- variable3.sh
+++ variable3.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-START_TIME=$(date)
+START_TIME=$(date +%s)
 echo "Current Timestamp of script executed: $START_TIME"
 
 
 sleep 10
 
-END_TIME=$(date)
+END_TIME=$(date  date +%s)
+TOTAL_TIME=$((END_TIME - START_TIME))
+
+echo "Total time taken to execute the script: $TOTAL_TIME seconds"      
