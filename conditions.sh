4192e23ab11bbdacbe6054725b08ab1b6f509082
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-29 02:02:50 -0500

    learning

--- conditions.sh
+++ conditions.sh
@@ -2,13 +2,13 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 20 ]; then
+if [ "$NUMBER" -gt 20 ]; then
 echo " Given number: $NUMBER is greater than 20"
 
-elif [ $NUMBER -lt 20 ] ; then
+elif [ "$NUMBER" -lt 20 ] ; then
 echo "given number : $NUMBER is less than 20"
 
-elif [ $NUMBER -eq 20 ] ; then
+elif [ "$NUMBER" -eq 20 ] ; then
 echo " Given number: $NUMBER is equal to 20"
 else
 echo " Please provide a valid number"
