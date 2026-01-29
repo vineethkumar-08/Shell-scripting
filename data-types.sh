fc832dcb448f835c944c72f0265d32586e11da46
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-29 01:06:41 -0500

    learning

--- data-types.sh
+++ data-types.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 Num1=100
-Num2=vineeth
+Num2=200
 
 SUM=$((Num1+Num2)) 
 echo "Sum is : $SUM"
+
+
+#Array
+FRUITS= (" APPLE" " BANANA" " ORANGE" " GRAPES")
+
+echo "Fruits are : ${FRUITS[@]}"
