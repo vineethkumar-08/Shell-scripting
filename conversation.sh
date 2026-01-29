27a52cdab0972cb9bbd30322f3ddc4b0898735b7
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-28 23:03:34 -0500

    learning

--- conversation.sh
+++ conversation.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-echo "vineeth : hi, how are you?"
-echo "chintu : i am fine, thank you! how about you?"
-echo "vineeth : i am doing well, thanks for asking."    
-echo "chintu : great to hear that. what have you been up to lately?"
-echo "vineeth : i've been working on some shell scripting projects. it's been fun!"
-echo "chintu : that's awesome! shell scripting is really useful for automating tasks."
-echo "vineeth : absolutely! do you have any projects you're working on?"
-echo "chintu : yes, i'm trying to create a backup script for my important files."
-echo "vineeth : sounds like a great idea. let me know if you need any help with it."
-echo "chintu : will do! thanks, vineeth."
+
+PERSON1="Vineeth"
+PERSON2="Chintu"
+
+
+echo "$PERSON1 : hi, how are you?"
+echo "$PERSON2 : i am fine, thank you! how about you?"
+echo "$PERSON1 : i am doing well, thanks for asking."    
+echo "$PERSON2 : great to hear that. what have you been up to lately?"
+echo "$PERSON1 : i've been working on some shell scripting projects. it's been fun!"
+echo "$PERSON2 : that's awesome! shell scripting is really useful for automating tasks."
+echo "$PERSON1 : absolutely! do you have any projects you're working on?"
+echo "$PERSON2 : yes, i'm trying to create a backup script for my important files."
+echo "$PERSON1 : sounds like a great idea. let me know if you need any help with it."
+echo "$PERSON2 : will do! thanks, vineeth."
