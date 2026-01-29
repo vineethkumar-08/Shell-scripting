a371419532a38d637842e959a87bf8adafbba8a8
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-28 23:46:18 -0500

    learning

--- variables1.sh
+++ variables1.sh
@@ -7,4 +7,4 @@ echo "user name is $USER_NAME"
    echo : Please enter your password
    read -s PASSWORD
 
-   echo "password is $PASSWORD"
+   echo "password is $PASSWORD"  #displaying password for demo purpose only no need to keep while using in real time scenarios
---
+++ variables2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+TIMESTAMP=$(date)
+echo "Current Timestamp of script executed: $TIMESTAMP"
