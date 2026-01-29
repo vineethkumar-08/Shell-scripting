e3b705ae1046a4e33dcc59cf9e9414ea7708e6f7
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-29 01:14:29 -0500

    learning

--- data-types.sh
+++ data-types.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# sum of the two numbers
 Num1=100
 Num2=200
 
@@ -10,4 +10,7 @@ echo "Sum is : $SUM"
 #Array
 FRUITS=("APPLE" "BANANA" "ORANGE" "GRAPES")
 
-echo "Fruits are : ${FRUITS[@]}"
+echo "Fruits are : ${FRUITS[@]}"#display all array elements
+echo "First Fruit is : ${FRUITS[0]}" #display first element
+echo "Second Fruit is : ${FRUITS[1]}" #display second element
+echo "Total number of fruits: ${#FRUITS[@]}" #display total number of elements in array
