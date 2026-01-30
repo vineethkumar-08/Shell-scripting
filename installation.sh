31faa1477cfb893da89a7326132b87a8a112b7ac
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-30 01:04:53 -0500

    learning

--- installation.sh
+++ installation.sh
@@ -12,3 +12,11 @@ fi
 echo "installing nginx web server"
 
 dnf install nginx -y
+
+if [ $? -ne 0 ] ; then
+echo " nginx installation failed..."
+ecit 1
+else
+
+echo " Nginx Instalation sucessful...."
+fi
