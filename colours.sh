385cbf0385d815581393d264c09e19f0f400a2f5
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-30 14:29:20 -0500

    learning

---
+++ colours.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+R=\e[31m # Red
+G=\e[32m # Green
+Y=\e[33m    # Yellow
+B=\e[34m # Blue
+M=\e[35m # Magenta
+C=\e[36m # Cyan
+W=\e[37m  # White
+N=\e[0m  # No Color
+
+
+echo -e $G "Hello this is vineeth" $N " Am learning colours in shell scripting"]
---
+++ loop-modified.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/Shell-scripting"  #defining logs folder path
+LOGS_FILE="/var/logs/Shell-scripting/$0.log"   #defining log file path
+
+if [ $USERID -ne 0 ] ; then 
+
+echo " Please run this script with root acess" | tee -a $LOGS_FILE     #run as root user 
+
+exit  # exit scode
+fi
+mkdir -p $LOGS_FOLDER  #creating logs folder if not exists
+
+# Function to validate the installation status
+VALIDATE(){
+     
+    if [ $1 -ne 0 ] ; then
+        echo " $2 installation failed..." | tee -a $LOGS_FILE # error message 
+        exit 1
+    else
+        echo " $2 Instalation sucessful...."  | tee -a $LOGS_FILE
+fi
+
+
+}
+  for PACKAGE in $@ #loop through all passed arguments like sh logs.sh nginx mysql nodejs
+  do
+     dnf list installed $PACKAGE  &>> $LOGS_FILE
+     if $? -ne 0 ; then 
+     echo " $package  not installed , installing now..." | tee -a $LOGS_FILE
+    dnf install $PACKAGE -y &>> $LOGS_FILE
+    VALIDATE $? " insstalling $PACKAGE web server" | tee -a $LOGS_FILE
+    else
+    echo " $PACKAGE is already installed , skipping installation..." | tee -a $LOGS_FILE
+    fi  
+    done 
