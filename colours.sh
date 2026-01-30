b4b2d3de07853b5ba432b16ff6e216842e96ca41
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-30 15:39:28 -0500

    learning

--- colours.sh
+++ colours.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-R=\e[31m # Red
-G=\e[32m # Green
-Y=\e[33m    # Yellow
-B=\e[34m # Blue
-M=\e[35m # Magenta
-C=\e[36m # Cyan
-W=\e[37m  # White
-N=\e[0m  # No Color
+R="\e[31m" # Red
+G="\e[32m" # Green
+Y="\e[33m"    # Yellow
+B="\e[34m" # Blue
+M="\e[35m" # Magenta
+C="\e[36m" # Cyan
+W="\e[37m"  # White
+N="\e[0m"  # No Color
 
 
-echo -e "$G Hello this is vineeth $N, $Y Am learning colours in shell scripting $N"
+echo -e "$R Hello this is vineeth $N, $Y Am learning colours in shell scripting $N"
