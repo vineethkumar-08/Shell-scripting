c204be3a4db998a6af0a2fed08c17003f5d10616
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-30 10:32:56 -0500

    learning

--- logs.sh
+++ logs.sh
@@ -6,7 +6,7 @@ LOGS_FILE="/var/logs/Shell-scripting/$0.log"   #defining log file path
 
 if [ $USERID -ne 0 ] ; then 
 
-echo " Please run this script with root acess" #run as root user
+echo " Please run this script with root acess" | tee -a $LOGS_FILE     #run as root user 
 
 exit  # exit scode
 fi
@@ -16,18 +16,18 @@ mkdir -p $LOGS_FOLDER  #creating logs folder if not exists
 VALIDATE(){
      
     if [ $1 -ne 0 ] ; then
-        echo " $2 installation failed..." # error message 
+        echo " $2 installation failed..." | tee -a $LOGS_FILE # error message 
         exit 1
     else
-        echo " $2 Instalation sucessful...."
+        echo " $2 Instalation sucessful...."  | tee -a $LOGS_FILE
 fi
 }
 
   dnf install nginx -y &>> $LOGS_FILE
-    VALIDATE $? " insstalling nginx web server"
+    VALIDATE $? " insstalling nginx web server" | tee -a $LOGS_FILE
 
     dnf install mysql -y &>> $LOGS_FILE
-    VALIDATE $? " insstalling mysql database server"
+    VALIDATE $? " insstalling mysql database server" | tee -a $LOGS_FILE
 
     dnf install nodejs -y &>> $LOGS_FILE
-    VALIDATE $? " insstalling nodejs web server"
+    VALIDATE $? " insstalling nodejs web server" | tee -a $LOGS_FILE
